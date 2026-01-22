8acc092072fd93c7989103f047adf8c029ef47b5
sailendraprasath <sailendraprasath05@gmail.com>
2026-01-22 05:41:41 +0530

    Fix staticfiles storage crash on Render

--- attendance_project/settings.py
+++ attendance_project/settings.py
@@ -49,7 +49,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
